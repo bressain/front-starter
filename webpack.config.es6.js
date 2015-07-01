b4ea8f6bb113416090b17fd2ad8bf08bec8b6ba6
Bressain Dinkelman <dinkelburt@gmail.com>
2015-07-01 15:37:18 -0600

    Initial commit

---
+++ webpack.config.es6.js
@@ -0,0 +1,33 @@
+import autoprefixer from 'autoprefixer'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+export default {
+  entry: [
+    'babel/polyfill',  // initialize babel/es6 environment first
+    './lib/index.js'
+  ],
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  module:{
+    loaders: [
+      { test: /\.js$/, loader: 'babel?stage=0', exclude: /node_modules/ },  // run through babel with stage 0 (experimental es7) features
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css!postcss!cssnext') }  // allow use of CSS4 syntax through postcss plugin system with cssnext
+    ]
+  },
+  postcss: [ autoprefixer ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.html',
+      title: 'React Starter',
+      appMountId: 'app',
+      devServer: 'http://localhost:3000'
+    }),
+    new ExtractTextPlugin(null, 'bundle.css')  // extract all css into a file instead of inlining into the head
+  ],
+  devServer: {
+    port: 3000
+  }
+}
